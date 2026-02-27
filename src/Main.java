ce5e7672d40faec8b269a913a17600763ef67959
Clementterme <clement.terme.simplon@gmail.com>
2026-02-27 12:29:18 +0100

    Vérification du type d'entrées clavier

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,25 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner clavier = new Scanner(System.in);
+
         System.out.print("Saisir le nombre de cartons à transporter : ");
+
+        while (!clavier.hasNextInt()) {
+            System.out.println("Veuillez entrer un nombre.");
+            clavier.nextLine();
+        }
+
         int cartons = clavier.nextInt();
+
         Scanner clavier2 = new Scanner(System.in);
+
         System.out.print("Saisir la capacité du camion : ");
+
+        while (!clavier2.hasNextInt()) {
+            System.out.println("Veuillez entrer un nombre.");
+            clavier2.nextLine();
+        }
+
         int capaciteCamion = clavier2.nextInt();
 
         while (cartons != 0) {
@@ -20,3 +35,5 @@ public class Main {
         }
     }
 }
+
+// Entrées doivent être nombres et différents de 0
