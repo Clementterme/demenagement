c290ee49dd5fbfb4bd0fc3675227d66460d0b192
Clementterme <clement.terme.simplon@gmail.com>
2026-02-27 11:54:14 +0100

    Dynamique avec entrées clavier

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     static void main() {
-        int cartons = 34;
-        for (int i = 0; i <= cartons; i++) {
-            if (cartons > 9) {
-                System.out.println("Un voyage de " + 9 + " cartons.");
-                cartons = cartons - 9;
+        Scanner clavier = new Scanner(System.in);
+        System.out.print("Saisir le nombre de cartons à transporter : ");
+        int cartons = clavier.nextInt();
+        Scanner clavier2 = new Scanner(System.in);
+        System.out.print("Saisir la capacité du camion : ");
+        int capaciteCamion = clavier2.nextInt();
+
+        while (cartons != 0) {
+            if (cartons >= capaciteCamion) {
+                System.out.println("Un voyage de " + capaciteCamion + " cartons.");
+                cartons = cartons - capaciteCamion;
             } else {
                 System.out.println("Un voyage de " + cartons + " cartons.");
                 cartons = 0;
